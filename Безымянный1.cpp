d3bc6cf85017127197e368698539e0c74d156673
fil-ondre <filipilila87@gmail.com>
2017-05-03 00:51:03 +0700

    hello-world

---
+++ Безымянный1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstdlib> // ??? system
+using namespace std;
+
+int main() 
+{ 
+    cout << "Hello, world!" << endl;
+    system("pause"); // ?????? ??? ???, ? ???? MS Visual Studio
+    return 0; 
+}
